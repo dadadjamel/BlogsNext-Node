4b3b56fed1ad498f6e9eb5a62c5e88cc3b2630a4
Djamel <gd_dada@esi.dz>
2024-10-14 21:14:53 +0200

    deploy no type

--- frontend/my-app/next.config.mjs
+++ frontend/my-app/next.config.mjs
@@ -4,16 +4,23 @@ const nextConfig = {
     swcMinify: true,
     // Add image domains to support external images
     images: {
-      domains: ['via.placeholder.com'], // Add the required domain here
+        domains: ['via.placeholder.com'], // Add the required domain here
     },
     // except for webpack, other parts are left as generated
     webpack: (config, context) => {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-      return config;
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        };
+        return config;
     },
-  };
-  
-  export default nextConfig;
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+};
+
+export default nextConfig;
